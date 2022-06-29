f2af60da186e73a02fd50b32b41d782e7f449a7a
Yevhenii Pozniak <jekamanu@gmail.com>
2022-06-29 13:17:59 +0300

    update timer task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,96 +4,8 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 import { Notify } from 'notiflix';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
-// const refs = {
-//   input: document.querySelector('#datetime-picker'),
-//   startBtn: document.querySelector('button[data-start]'),
-//   days: document.querySelector('span[data-days]'),
-//   hours: document.querySelector('span[data-hours]'),
-//   minutes: document.querySelector('span[data-minutes]'),
-//   seconds: document.querySelector('span[data-seconds]'),
-//   output: document.querySelector('.timer'),
-// };
-
-// refs.output.style.display = 'flex';
-
-// const isDisabled = true;
-// refs.startBtn.disabled = isDisabled;
-// let intervalId = null;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     dateCheck(selectedDates[0]);
-//   },
-// };
-
-// const timePicker = flatpickr(refs.input, options);
-
-// function dateCheck(date) {
-//   const currentDate = new Date();
-
-//   if (currentDate > date) {
-//     Notify.failure('Please choose date in the future');
-//   } else {
-//     refs.startBtn.disabled = !isDisabled;
-//   }
-// }
-
-// refs.startBtn.addEventListener('click', onStart);
-
-// function onStart() {
-//   refs.startBtn.disabled = isDisabled;
-//   refs.input.disabled = isDisabled;
-//   countdown();
-// }
-
-// function countdown() {
-//   intervalId = setInterval(() => {
-//     const diff = timePicker.selectedDates[0] - Date.now();
-//     const convertedTime = convertMs(timePicker.selectedDates[0] - Date.now());
-//     updateTimerValues(convertedTime);
-
-//     if (diff < 1000) {
-//       clearInterval(intervalId);
-
-//       Notify.success('Ура!');
-//     }
-//   }, 1000);
-// }
-
-// function addZero(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// function updateTimerValues({ days, hours, minutes, seconds }) {
-//   refs.days.textContent = addZero(days);
-//   refs.hours.textContent = addZero(hours);
-//   refs.minutes.textContent = addZero(minutes);
-//   refs.seconds.textContent = addZero(seconds);
-// }
 const refs = {
   buttonStart: document.querySelector('[data-start]'),
   input: document.querySelector('#datetime-picker'),
@@ -120,7 +32,7 @@ const options = {
       return Notify.failure('Please choose a date in the future');
     }
 
-    Notify.success('Date is valid, click to Start button');
+    Report.success('Date is valid, click to Start button');
     refs.buttonStart.removeAttribute('disabled');
   },
 };
@@ -139,7 +51,7 @@ class Timer {
       if (selectedDate - Date.now() <= 0) {
         clearInterval(this.intervalID);
         refs.input.removeAttribute('disabled');
-        return Report.success('SALE STARTED!!!', 'LET`S GO', 'Okay');
+        return Notify.success('SALE STARTED!!!', 'LET`S GO', 'Okay');
       }
       const deltaTime = selectedDate - Date.now();
       const formatedDate = this.convertMs(deltaTime);
